f6d94cf3cc065c1cb45caf2d2204e04eaba99783
Michael MacLeod <michaelmmacleod@gmail.com>
2023-10-21 08:13:01 -0700

    Remove intellij stuff

--- implementation/intellij.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-nix shell \
-    nixpkgs#rustc \
-    nixpkgs#cargo \
-    nixpkgs#gcc \
-    nixpkgs#jetbrains.idea-community \
-    -c idea-community
--- implementation/patch-intellij-rust-native-helper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Script to patch intellij-rust-native-helper on NixOS.
-
-set -e
-
-nix shell nixpkgs#fd nixpkgs#patchelf --command sh -c "./patch-intellij-rust-native-helper.impl.sh ${1}"
