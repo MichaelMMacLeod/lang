fc753a4ae27cc8720f8e64e38713a25e708812a4
Michael MacLeod <michaelmmacleod@gmail.com>
2023-10-21 08:09:19 -0700

    Get a reasonable flake.nix working

--- implementation/flake.nix
+++ implementation/flake.nix
@@ -1,25 +1,37 @@
-{ self, ... }: {
-  description "devshell";
+{
+  description = "development environment for lang";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05";
-
-  outputs = { self, nixpkgs }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      eachSystem (system: {
-        devShell = pkgs.mkShell {
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in {
-            buildInputs = with pkgs; [
-              rustc
-              cargo
-              gcc
-              jetbrains.idea-community
-            ];
-          };
-        };
-      });
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            (vscode-with-extensions.override {
+              vscode = vscodium;
+              vscodeExtensions = with vscode-extensions; [
+                rust-lang.rust-analyzer
+              ];
+            })
+          ];
+        };
+      }
+    );
 }
