23edde8804b32592c7995eb16b7696413f3de247
Michael MacLeod <michaelmmacleod@gmail.com>
2023-10-22 05:38:57 -0700

    Work on doc more

--- implementation/src/scoped_term.rs
+++ implementation/src/scoped_term.rs
@@ -12,6 +12,19 @@ pub struct ScopedTerm {
     pub unscoped_term: UnscopedTerm,
 }
 
+impl ScopedTerm {
+    // pub fn binds(&self, other: &Self) -> Option<> {
+    //     // $x <singular|compound> --> (Some (bind $x <singular|compound>))
+    //     // <compound len=l1> <compound len=l2>
+
+    //     // Doesn't bind if scopes don't match
+    //     // case 1: $x and <T> --> bind $x to <T>
+    //     // case 2: <X> and <T> for compound <X>, <T>: binds iff <X> and <T> have same # of subterms
+    //     // case 3: <X> and <T> where 1 is singular and other is compound: doesn't match
+    //     // case 4: <X> ... and 
+    // }
+}
+
 impl std::fmt::Debug for ScopedTerm {
     fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         write!(fmt, "{:?}", self.unscoped_term)
--- implementation/src/term_match.rs
+++ implementation/src/term_match.rs
@@ -10,16 +10,26 @@ pub struct RuleMatch {
 
 impl RuleMatch {
     fn match_custom_rule(r: &CustomRule, tg: &TermGraph, term: Term) -> Option<Self> {
+        // $t singular
+        // -->
+        // $t=singular
+        //
+        // singular1 singular2
+        // -->
+        // when singular1==singular2, $t=singular1 
         let mut pattern_bfs = Bfs::new(&r.pattern.graph, r.pattern.redex);
         let mut tg_bfs = Bfs::new(&tg.graph, term);
         let mut bindings = vec![];
+        let mut entered_graphs = false;
         let mut matched = false;
-        while let (Some(p), Some(t)) = (pattern_bfs.next(&r.pattern.graph), tg_bfs.next(&tg.graph)) {
+        while let (Some(p), Some(t)) = (pattern_bfs.next(&r.pattern.graph), tg_bfs.next(&tg.graph))
+        {
+            entered_graphs = true;
             let (sr, st) = (&r.pattern.graph[p], &tg.graph[t]);
             if sr.scope_set.binds(&st.scope_set) {
-                matched = true;
             }
         }
+        assert!(entered_graphs);
         if matched {
             Some(Self { bindings })
         } else {
@@ -31,6 +41,15 @@ impl RuleMatch {
 pub struct Binding {
     identifier: String,
     bound_terms: BoundTerms,
+}
+
+impl Binding {
+    pub fn new(identifier: String, bound_terms: BoundTerms) -> Self {
+        Self {
+            identifier,
+            bound_terms,
+        }
+    }
 }
 
 pub struct BoundTerms {
