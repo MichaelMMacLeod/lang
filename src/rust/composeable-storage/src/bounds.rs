93560b1a3da456933e58443566c1ab7dbfd0dfb2
Michael MacLeod <michaelmmacleod@gmail.com>
2023-11-13 21:18:31 -0800

    Update bounds a bit

--- src/rust/composeable-storage/src/bounds.rs
+++ src/rust/composeable-storage/src/bounds.rs
@@ -1,4 +1,4 @@
-use num_traits::Zero;
+use num_traits::{Zero, Unsigned};
 
 use crate::{
     merge::TryMerge,
@@ -17,13 +17,13 @@ pub struct AtOrBelow<T: Boundable>(pub T);
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)] // no Default
 pub struct AtOrInside<T: Boundable> {
     // invariant: at_or_above <= at_or_below
-    at_or_above: T,
-    at_or_below: T,
+    at_or_above: AtOrAbove<T>,
+    at_or_below: AtOrBelow<T>,
 }
 
 impl<T: Boundable> AtOrInside<T> {
-    pub fn try_new(at_or_above: T, at_or_below: T) -> Option<Self> {
-        (at_or_above <= at_or_below).then(|| Self {
+    pub fn try_new(at_or_above: AtOrAbove<T>, at_or_below: AtOrBelow<T>) -> Option<Self> {
+        (at_or_above.0 <= at_or_below.0).then(|| Self {
             at_or_above,
             at_or_below,
         })
@@ -32,80 +32,86 @@ impl<T: Boundable> AtOrInside<T> {
 
 impl<T: Boundable> From<AtOrInside<T>> for (AtOrAbove<T>, AtOrBelow<T>) {
     fn from(value: AtOrInside<T>) -> Self {
-        (AtOrAbove(value.at_or_above), AtOrBelow(value.at_or_below))
+        (value.at_or_above, value.at_or_below)
     }
 }
 
+pub trait Offsettable: Boundable + Unsigned {}
+impl<O: Boundable + Unsigned> Offsettable for O {}
+
 #[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub struct OffsetAtOrAbove<T: Boundable> {
+pub struct Offset<O: Offsettable>(pub O);
+
+#[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct OffsetAtOrAbove<O: Offsettable> {
     // invariant: offset >= at_or_above
-    offset: T,
-    at_or_above: T,
+    offset: Offset<O>,
+    at_or_above: AtOrAbove<O>,
 }
 
-impl<T: Boundable> OffsetAtOrAbove<T> {
-    pub fn try_new(offset: T, at_or_above: T) -> Option<Self> {
-        (offset >= at_or_above).then(|| Self {
+impl<O: Offsettable> OffsetAtOrAbove<O> {
+    pub fn try_new(offset: Offset<O>, at_or_above: AtOrAbove<O>) -> Option<Self> {
+        (offset.0 >= at_or_above.0).then(|| Self {
             offset,
             at_or_above,
         })
     }
 }
 
-impl<T: Boundable> From<OffsetAtOrAbove<T>> for (T, AtOrAbove<T>) {
-    fn from(value: OffsetAtOrAbove<T>) -> Self {
-        (value.offset, AtOrAbove(value.at_or_above))
+impl<O: Offsettable> From<OffsetAtOrAbove<O>> for (Offset<O>, AtOrAbove<O>) {
+    fn from(value: OffsetAtOrAbove<O>) -> Self {
+        (value.offset, value.at_or_above)
     }
 }
 
-#[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub struct OffsetAtOrBelow<T: Boundable> {
-    // invariant: offset <= at_or_below
-    offset: T,
-    at_or_below: T,
-}
+// #[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+// pub struct OffsetAtOrBelow<T: Boundable> {
+//     // invariant: offset <= at_or_below
+//     offset: T,
+//     at_or_below: T,
+// }
 
-impl<T: Boundable> OffsetAtOrBelow<T> {
-    pub fn try_new(offset: T, at_or_below: T) -> Option<Self> {
-        (offset <= at_or_below).then(|| Self {
-            offset,
-            at_or_below,
-        })
-    }
-}
+// impl<T: Boundable> OffsetAtOrBelow<T> {
+//     pub fn try_new(offset: T, at_or_below: T) -> Option<Self> {
+//         (offset <= at_or_below).then(|| Self {
+//             offset,
+//             at_or_below,
+//         })
+//     }
+// }
 
-impl<T: Boundable> From<OffsetAtOrBelow<T>> for (T, AtOrBelow<T>) {
-    fn from(value: OffsetAtOrBelow<T>) -> Self {
-        (value.offset, AtOrBelow(value.at_or_below))
-    }
-}
+// impl<T: Boundable> From<OffsetAtOrBelow<T>> for (T, AtOrBelow<T>) {
+//     fn from(value: OffsetAtOrBelow<T>) -> Self {
+//         (value.offset, AtOrBelow(value.at_or_below))
+//     }
+// }
 
-impl<T: PartialOrd + Zero + Copy> Partition<AtOrInside<T>> for OffsetAtOrAbove<T> {
-    fn partition(self) -> Partitioned<AtOrInside<T>, Self> {
-        let data = AtOrInside {
-            at_or_above: self.at_or_above,
-            at_or_below: self.offset,
-        };
-        let mut zero = self.offset.clone();
-        zero.set_zero();
-        let storage = OffsetAtOrAbove {
-            at_or_above: self.offset,
-            offset: zero,
-        };
-        Partitioned::new(data, storage)
-    }
-}
+// impl<T: PartialOrd + Zero + Copy> Partition<AtOrInside<T>> for OffsetAtOrAbove<T> {
+//     fn partition(self) -> Partitioned<AtOrInside<T>, Self> {
+//         let data = AtOrInside {
+//             at_or_above: self.at_or_above,
+//             at_or_below: self.offset,
+//         };
+//         let mut zero = self.offset.clone();
+//         zero.set_zero();
+//         let storage = OffsetAtOrAbove {
+//             at_or_above: self.offset,
+//             offset: zero,
+//         };
+//         Partitioned::new(data, storage)
+//     }
+// }
 
-#[cfg(test)]
-mod test {
-    use crate::partition::Partition;
+// #[cfg(test)]
+// mod test {
+//     use crate::partition::Partition;
 
-    use super::OffsetAtOrAbove;
+//     use super::OffsetAtOrAbove;
 
-    #[test]
-    fn partition1() {
-        let aoo1 = OffsetAtOrAbove::try_new(64, 0).unwrap();
-        let (aoi, aoo2) = aoo1.partition().into();
-        let (at_or_above, at_or_below) = aoi.into();
-    }
-}
+//     #[test]
+//     fn partition1() {
+//         let aoo1 = OffsetAtOrAbove::try_new(64, 0).unwrap();
+//         let (aoi, aoo2) = aoo1.partition().into();
+//         let (at_or_above, at_or_below) = aoi.into();
+//     }
+// }
