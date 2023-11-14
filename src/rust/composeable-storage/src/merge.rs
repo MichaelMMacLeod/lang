e783eb41d8e1f22492c56b05b195a716a33784cd
Michael MacLeod <michaelmmacleod@gmail.com>
2023-11-13 21:27:17 -0800

    Add more merge traits

--- src/rust/composeable-storage/src/merge.rs
+++ src/rust/composeable-storage/src/merge.rs
@@ -1,6 +1,15 @@
+pub trait Merge<Data>: Sized {
+    fn merge(self, data: Data) -> Self;
+}
+
 pub trait TryMerge<Data>: Sized {
     type TryMergeError;
     fn try_merge(self, data: Data) -> Result<Self, Self::TryMergeError>;
+}
+
+pub trait MergeUnsafe<Data>: Sized {
+    type MergeUnsafeError;
+    unsafe fn merge_unsafe(self, data: Data) -> Self;
 }
 
 pub trait TryMergeUnsafe<Data>: Sized {
