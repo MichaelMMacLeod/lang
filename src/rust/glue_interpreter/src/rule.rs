74ee5d6944928ad2cba933b88a0e43a206558f80
Michael MacLeod <michaelmmacleod@gmail.com>
2023-12-14 21:53:11 -0800

    Basic rule matching

--- src/rust/glue_interpreter/src/rule.rs
+++ src/rust/glue_interpreter/src/rule.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::storage::{Storage, StorageKey, Term};
 
@@ -30,6 +30,10 @@ struct Many {
     sp: SinglePattern,
     mp: MultiPattern,
 }
+
+// TODO:
+// - verify that every variable is not used more than once
+// - verify that in "T ..", T must contain at least one variable
 
 fn parse_rule_pattern_single(
     storage: &Storage,
@@ -86,22 +90,128 @@ fn parse_rule_pattern_multi(
     }
 }
 
+#[derive(Hash, PartialEq, Eq, Debug, Clone)]
+enum Match {
+    Leaf(StorageKey),
+    Branches(Vec<Match>),
+}
+
+fn pattern_match_single(
+    storage: &Storage,
+    pattern: &SinglePattern,
+    k: StorageKey,
+) -> Option<HashMap<String, Match>> {
+    match pattern {
+        SinglePattern::Variable(v) => Some(HashMap::from([(v.clone(), Match::Leaf(k))])),
+        SinglePattern::Symbol(s1) => {
+            if let Term::Symbol(s2) = storage.get(k).unwrap() {
+                if s1 == s2.data() {
+                    Some(HashMap::new())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        }
+        SinglePattern::Compound(mp) => {
+            if let Term::Compound(c) = storage.get(k).unwrap() {
+                pattern_match_multi(storage, mp, c.keys())
+            } else {
+                None
+            }
+        }
+    }
+}
+
+fn pattern_match_multi(
+    storage: &Storage,
+    pattern: &MultiPattern,
+    ks: &[StorageKey],
+) -> Option<HashMap<String, Match>> {
+    match pattern {
+        MultiPattern::Nothing => ks.is_empty().then(HashMap::new),
+        MultiPattern::One(one) => {
+            if let Some(k) = ks.get(0) {
+                let One { sp, mp } = one.as_ref();
+                pattern_match_single(storage, sp, *k)
+                    .map(|matches1| {
+                        pattern_match_multi(storage, mp, &ks[1..]).map(|matches2| {
+                            let mut h = HashMap::new();
+                            h.extend(matches1.into_iter());
+                            h.extend(matches2.into_iter());
+                            h
+                        })
+                    })
+                    .flatten()
+            } else {
+                None
+            }
+        }
+        MultiPattern::Many(many) => {
+            let Many { sp, mp } = many.as_ref();
+            let sp_matches = ks
+                .iter()
+                .map_while(|k| pattern_match_single(storage, sp, *k))
+                .collect::<Vec<_>>();
+            let combined_sp_matches = if !sp_matches.is_empty() {
+                let mut branches: HashMap<String, Vec<Match>> = HashMap::new();
+                for var in sp_matches[0].keys() {
+                    for ht in sp_matches.iter() {
+                        for (var2, m) in ht {
+                            if var == var2 {
+                                branches
+                                    .entry(var.clone())
+                                    .and_modify(|bs| bs.push(m.clone()))
+                                    .or_insert(vec![m.clone()]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+                branches
+                    .into_iter()
+                    .map(|(k, v)| (k, Match::Branches(v)))
+                    .collect()
+            } else {
+                HashMap::new()
+            };
+            pattern_match_multi(storage, mp, &ks[sp_matches.len()..]).map(|mp_matches| {
+                let mut h = HashMap::new();
+                h.extend(combined_sp_matches.into_iter());
+                h.extend(mp_matches.into_iter());
+                h
+            })
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{parser::parse, lexer::lex};
+    use crate::{lexer::lex, parser::parse};
 
     use super::*;
 
     #[test]
     fn parse_rule_pattern_single1() {
         let mut s = Storage::new();
-        let k = parse(
-            &mut s,
-            lex("(flatten (list (list xs ..) ..))").unwrap().1,
-        );
+        let k = parse(&mut s, lex("(flatten (list (list xs ..) ..))").unwrap().1);
         let mut vs = HashSet::new();
         vs.insert("xs".into());
         let pattern = parse_rule_pattern_single(&s, k, &vs);
         dbg!(pattern);
+    }
+
+    #[test]
+    fn pattern_match_single1() {
+        let mut s = Storage::new();
+        let k = parse(&mut s, lex("(flatten (list (list xs ..) ..))").unwrap().1);
+        let mut vs = HashSet::new();
+        vs.insert("xs".into());
+        let pattern = parse_rule_pattern_single(&s, k, &vs);
+        let value_k = parse(&mut s, lex("(flatten (list (list a b c) (list d e) (list)))").unwrap().1);
+        let m = pattern_match_single(&s, &pattern, value_k);
+        dbg!(m);
     }
 }
